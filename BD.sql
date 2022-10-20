93afdac0b264868eb3263516b16e90fa5f97c663
FabricioTravalha <115188409+FabricioTravalha@users.noreply.github.com>
2022-10-19 22:32:30 -0300

    Banco de dados
    Arquivo do banco de dados do MySQL. Conexão feita pelo MySQL Workcench com:
    Host: localhost
    Usuario: root
    Password: deixar em branco
    Nome do banco: cadastro

---
+++ BD.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE  IF NOT EXISTS `cadastro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci */;
+USE `cadastro`;
+-- MariaDB dump 10.19  Distrib 10.4.25-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: cadastro
+-- ------------------------------------------------------
+-- Server version	10.4.25-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `telefone` varchar(45) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `senha` varchar(45) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `data_nasc` date NOT NULL,
+  `genero` varchar(45) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `prof` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Fabricio Travalha','fabricio_daniel16@hotmail.com','(47)99877-8457','asdasda','2022-10-04','masc','4'),(2,'Doraci','aaaa@aaa.com','(47)98748-5255','dddddddd','2022-10-19','femin','10'),(3,'Djonathan Busareloo','busa@busa.com','(47)99568-2211','dasdasda','2022-10-04','Prefiro não responder','0'),(4,'Djonathan Busareloo','busa@busa.com','(47)99568-2211','dasdasda','2022-10-04','Prefiro não responder','Uso a ferramenta casualmente e quero aprender'),(5,'Djonathan Busareloo','busa@busa.com','(47)99568-2211','dasdasd','2022-10-10','Prefiro não responder','Tenho conhecimento mas quero aprender mais');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'cadastro'
+--
+
+--
+-- Dumping routines for database 'cadastro'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-19 21:42:50
